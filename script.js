96262777963ef7dd573d48a07bae98457b8b8a9c
pasoldne <vitorpasold4@gmail.com>
2025-09-11 19:31:03 -0300

    type OF

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+// console.log("Hello, World!");
+
+// let idade = 16;
+// let nome = "Vitor";
+// let cidade = "Pomerode";
+// let ehEstudante = true;
+// let altura = 1.86;
+
+// console.log("Nome: " + nome);
+// console.log("Idade: " + idade);
+// console.log("Cidade: " + cidade);
+// console.log("É estudante? " + ehEstudante);
+// console.log("Altura: " + altura + "m");
+
 console.log("Hello, World!");
+
 
 let idade = 16;
 let nome = "Vitor";
+console.log(typeof nome);
+
 let cidade = "Pomerode";
 let ehEstudante = true;
 let altura = 1.86;
+
+
+nome = 25;
+console.log(typeof nome);
+
+
 
 console.log("Nome: " + nome);
 console.log("Idade: " + idade);
