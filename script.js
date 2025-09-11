f7a95eac5b73a4df9a87fde658abd40400dc5854
pasoldne <vitorpasold4@gmail.com>
2025-09-11 19:39:25 -0300

    crase

--- script.js
+++ script.js
@@ -36,14 +36,26 @@
 // console.log("Altura: " + altura + "m");
 
 
-let texto = "Olá, Mundo!";
-let numero = 42;
-let booleano = true;
-let nulo = null;
-let indefinido;
+//let texto = "Olá, Mundo!";
+//let numero = 42;
+//let booleano = true;
+//let nulo = null;
+//let indefinido;
+//
+//console.log(typeof texto);        // string
+//console.log(typeof numero);       // number
+//console.log(typeof booleano);   // boolean
+//console.log(typeof nulo);         // object (this is a known quirk in //JavaScript)
+//console.log(typeof indefinido);   // undefined
 
-console.log(typeof texto);        // string
-console.log(typeof numero);       // number
-console.log(typeof booleano);   // boolean
-console.log(typeof nulo);         // object (this is a known quirk in JavaScript)
-console.log(typeof indefinido);   // undefined
+let nome = "Vitor";
+let idade = 16;
+let altura = 1.86;
+let ehEstudante = true;
+let endereco = "Pomerode, SC";
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Altura: ${altura}m`);
+console.log(`É estudante? ${ehEstudante}`);
+console.log(`Endereço: ${endereco}`);
