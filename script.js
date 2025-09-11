4b164b00ec2de7c8e458b26d31a5824637886bbc
pasoldne <vitorpasold4@gmail.com>
2025-09-11 19:24:55 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+let idade = 16;
+let nome = "Vitor";
+let cidade = "Pomerode";
+let ehEstudante = true;
+let altura = 1.86;
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("É estudante? " + ehEstudante);
+console.log("Altura: " + altura + "m");
+
